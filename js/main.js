85acdb14bd1bd5c6320476381d6b70ae14ce6537
Albey-Chowdhury <alveychow105@gmail.com>
2024-01-27 22:59:34 +0600

    remove js and css Modify

--- js/main.js
+++ js/main.js
@@ -13,26 +13,3 @@ function closesidebar(){
     sidebar.style.display = 'none'
 }
 
-
-
-
-
-
-// Get references to the car image and color buttons
-    const carImage = document.getElementById('car-image');
-    const redButton = document.getElementById('red');
-    const blueButton = document.getElementById('blue');
-    const greenButton = document.getElementById('green');
-
-    // Event listeners for color buttons
-    redButton.addEventListener('click', () => {
-        carImage.style.filter = 'hue-rotate(314deg)'; // Red color
-    });
-
-    blueButton.addEventListener('click', () => {
-        carImage.style.filter = 'hue-rotate(174deg)'; // Blue color
-    });
-
-    greenButton.addEventListener('click', () => {
-        carImage.style.filter = 'hue-rotate(53deg)'; // Green color
-    });
