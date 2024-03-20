c9edc90fa4b786bdb2cd316bdd042e63bd103533
Albey chowdhury <alveychow105@gmail.com>
2024-03-21 00:30:28 +0600

    delet some file

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-    $(".loader").fadeOut(4000);
-});
-
-//side bar 
-
 function showsidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'flex'
@@ -12,11 +6,3 @@ function closesidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
-
-
-// count timing start
-$(document).ready(function(){
-    $(".counter").counterUp({
-    });
-});
-// count timing end
